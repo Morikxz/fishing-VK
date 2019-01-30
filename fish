10d781155b2034fc68dc2ae138472770c2dce2e7
foxlitegor <46333757+foxlitegor@users.noreply.github.com>
2019-01-30 22:36:22 +0300

    Add files via upload

---
+++ fish
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cd /data/data/com.termux/files/usr/foxlite/fisher
+python2 fish.py
---
+++ install.sh
@@ -0,0 +1,9 @@
+cp fish /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+chmod 777 fish
+cd /data/data/com.termux/files/usr
+mkdir foxlite
+cd /data/data/com.termux/files/home/fisher
+cp -r fisher /data/data/com.termux/files/usr/foxlite
+cd /data/data/com.termux/files/usr/foxlite/fisher
+chmod 777 fish.py
